1f128d1be3b0cfe888fcca0298314805dfd860be
era9862 <44404407+era9862@users.noreply.github.com>
2026-08-12 13:43:32 +0000

    created band_name_generator.py file

---
+++ band_name_generator.py
@@ -0,0 +1,5 @@
+################################################################
+# band_name_generator.py                                       #
+# Creates a band name based on user's city and pet name input  #
+# @author: Elana Aronson                                       #
+################################################################
