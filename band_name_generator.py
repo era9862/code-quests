26b6894ccfa72f39a64991f7a04527345926c14d
era9862 <44404407+era9862@users.noreply.github.com>
2026-08-12 13:59:04 +0000

    added input handling and printed the band generator

--- band_name_generator.py
+++ band_name_generator.py
@@ -3,3 +3,15 @@
 # Creates a band name based on user's city and pet name input  #
 # @author: Elana Aronson                                       #
 ################################################################
+
+# Introduction to the generator
+print('Welcome to the Band Name Generator')
+
+# user's input of city
+city = input("What is the name of the city you live in? \n")
+
+# user's input of pet's name
+pet = input("What is the name of your pet? \n")
+
+# band generator
+print(f"Your band name could be {city} {pet}")
