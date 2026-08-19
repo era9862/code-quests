3bb19599c41591e16d62d65b8303dab78fca9e8b
era9862 <44404407+era9862@users.noreply.github.com>
2026-08-19 16:05:14 +0000

    Created rock paper scissor game file with random computer choice

---
+++ rock_paper_scissor.py
@@ -0,0 +1,16 @@
+################################################################
+# rock_paper_scissor.py                                        #
+# Game of Rock Paper Scissor                                   #
+# @author: Elana Aronson                                       #
+################################################################
+
+import random
+
+# Welcome message
+print("Welcome to the game of Rock Paper Scissor")
+
+# Computer randomly choose either rock, paper, or scissor
+computer_choice = random.choice(['rock', 'paper', 'scissor']) 
+
+# Start of the game
+user_choice = input
