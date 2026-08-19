6c59c938c11cca2db71119bcaeeb468f2b64f969
era9862 <44404407+era9862@users.noreply.github.com>
2026-08-19 13:47:16 +0000

    created treasure island quest game file

---
+++ treasure_island.py
@@ -0,0 +1,9 @@
+################################################################
+# treasure_island.py                                           #
+# Treasure Island game to complete a quest to find treasure:   #
+# If you go through the correct way, you win else game over    #
+# @author: Elana Aronson                                       #
+################################################################
+
+# Welcome message to the treasure quest game
+print("Welcome to Treasure Island.\nYour mission is to find the treasure")
